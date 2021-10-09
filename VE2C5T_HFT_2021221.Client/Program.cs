2251b0cbdb1e3f95bde2d15ce29c2047a295dcf6
szila <szila@DESKTOP-4KANCLM>
2021-10-09 05:32:21 +0200

    Add project files.

---
+++ VE2C5T_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VE2C5T_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VE2C5T_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VE2C5T_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
