4fcf7d3e220be34ea1790c1836e527e14210ef13
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-10-17 17:28:07 +0200

    Deleted the basic Class1 class and added 3 new classes (PetOwner, Pet, Vet) in .Models layer.

---
+++ VE2C5T_HFT_2021221.Models/Pet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VE2C5T_HFT_2021221.Models
+{
+    public class Pet
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string FavouriteFood { get; set; }
+
+        [ForeignKey(nameof(PetOwner))]
+        public int PetOwnerId { get; set; }
+
+        [NotMapped]
+        public virtual PetOwner PetOwner { get; set; }
+
+        [ForeignKey(nameof(Vet))]
+        public int VetId { get; set; }
+
+        [NotMapped]
+        public virtual Vet Vet { get; set; }
+
+
+
+    }
+}
---
+++ VE2C5T_HFT_2021221.Models/PetOwner.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VE2C5T_HFT_2021221.Models
+{
+    public class PetOwner
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string PhoneNumber  { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<Pet> Pets { get; set; }
+
+        public PetOwner()
+        {
+            Pets = new HashSet<Pet>();
+        }
+    }
+}
