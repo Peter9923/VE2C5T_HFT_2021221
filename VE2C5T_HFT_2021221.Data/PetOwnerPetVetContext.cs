4bc83e7b4e166e2d1e3a41196148c936f1f68f45
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-10-17 18:11:42 +0200

    In .Data layer: - Created 3 new DbSet<> (PetOwners, Pets, Vets) - Created an overrided method (OnConfiguring) - Installed 3 new nuget packages: Microsoft.EntityFrameworkCore.SqlServer Microsoft.EntityFrameworkCore.Tools Microsoft.EntityFrameworkCore.Proxies

--- VE2C5T_HFT_2021221.Data/PetOwnerPetVetContext.cs
+++ VE2C5T_HFT_2021221.Data/PetOwnerPetVetContext.cs
@@ -4,11 +4,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VE2C5T_HFT_2021221.Models;
 
 namespace VE2C5T_HFT_2021221.Data
 {
-    class PetOwnerPetVetContext : DbContext
+    public class PetOwnerPetVetContext : DbContext
     {
+        public virtual DbSet<PetOwner> PetOwners { get; set; }
+        public virtual DbSet<Pet> Pets { get; set; }
+        public virtual DbSet<Vet> Vets { get; set; }
 
+        public PetOwnerPetVetContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.
+                    UseLazyLoadingProxies().
+                    UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\MyDb.mdf;Integrated Security=True");
+            }
+        }
     }
 }
