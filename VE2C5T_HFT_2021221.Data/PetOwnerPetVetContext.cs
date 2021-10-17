a172cb221b9d1dccb16b86a51fcf0872c13789b9
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-10-17 17:49:27 +0200

    In .Data layer: - Deleted the basic Class1 class - Added a new class (PetOwnerPetVetContext) and this class is descendant the DbContext class - Installed Microsoft.EntityFrameworkCore nuget package

---
+++ VE2C5T_HFT_2021221.Data/PetOwnerPetVetContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VE2C5T_HFT_2021221.Data
+{
+    class PetOwnerPetVetContext : DbContext
+    {
+
+    }
+}
