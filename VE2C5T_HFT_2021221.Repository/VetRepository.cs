5120100b7729e45940a989aeb57fe3b4753829e1
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-11-12 20:36:17 +0100

    Created VetRepository class, with CRUD methods in the Repo layer.

---
+++ VE2C5T_HFT_2021221.Repository/VetRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VE2C5T_HFT_2021221.Data;
+using VE2C5T_HFT_2021221.Models;
+
+namespace VE2C5T_HFT_2021221.Repository
+{
+    class VetRepository : IVetRepository
+    {
+        MyDbContext context;
+
+        public VetRepository(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Vet vet)
+        {
+            context.Vets.Add(vet);
+            context.SaveChanges();
+        }
+
+        public Vet Read(int id)
+        {
+            return context
+               .Vets
+               .FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(Vet vet)
+        {
+            // regi objektum lekerdezese
+            Vet oldVet = Read(vet.Id);
+
+            // TODO: null check
+
+            if (oldVet == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            // tulajdonsagok felulirasa
+
+            oldVet.Name = vet.Name;
+            oldVet.PhoneNumber = vet.PhoneNumber;
+
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Vet vet = context
+                .Vets
+                .FirstOrDefault(x => x.Id == id);
+
+            context.Vets.Remove(vet);
+            context.SaveChanges();
+        }
+
+        public IQueryable<Vet> ReadAll()
+        {
+            return context.Vets;
+        }
+
+        
+    }
+}
