e667df15dd572fe778f0543a743fefb021ab5a0b
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-11-12 20:08:37 +0100

    Created IVetRepository interface, with five methods/functions. -Create -Read -ReadAll -Update -Delete

---
+++ VE2C5T_HFT_2021221.Repository/IVetRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VE2C5T_HFT_2021221.Models;
+
+namespace VE2C5T_HFT_2021221.Repository
+{
+    interface IVetRepository
+    {
+        void Create(Vet vet);
+        Vet Read(int id);
+        IQueryable<Vet> ReadAll();
+        void Update(Vet vet);
+        void Delete(int id);
+    }
+}
