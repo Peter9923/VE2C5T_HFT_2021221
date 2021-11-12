d8487ed9a277c2df52eae6905312dd6b11146dbb
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-11-12 20:20:10 +0100

    Created PetRepository class, with CRUD methods in the Repo layer.

---
+++ VE2C5T_HFT_2021221.Repository/PetRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VE2C5T_HFT_2021221.Data;
+using VE2C5T_HFT_2021221.Models;
+
+namespace VE2C5T_HFT_2021221.Repository
+{
+    class PetRepository : IPetRepository
+    {
+        MyDbContext context;
+
+        public PetRepository(MyDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Pet pet)
+        {
+            context.Pets.Add(pet);
+            context.SaveChanges();
+        }
+
+        public Pet Read(int id)
+        {
+            return context
+                .Pets
+                .FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Update(Pet pet)
+        {
+            // regi objektum lekerdezese
+            Pet oldPet = Read(pet.Id);
+
+            // TODO: null check
+
+            if (oldPet == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            // tulajdonsagok felulirasa
+
+            oldPet.Name = pet.Name;
+            oldPet.Species = pet.Species;
+            oldPet.PetOwnerId = pet.PetOwnerId;
+            oldPet.PetOwner = pet.PetOwner;
+            oldPet.VetId = pet.VetId;
+            oldPet.Vet = pet.Vet;
+        }
+
+        public void Delete(int id)
+        {
+            Pet pet = context
+                .Pets
+                .FirstOrDefault(x => x.Id == id);
+
+            context.Pets.Remove(pet);
+            context.SaveChanges();
+        }
+
+        public IQueryable<Pet> ReadAll()
+        {
+            return context.Pets;
+        }
+
+        
+    }
+}
