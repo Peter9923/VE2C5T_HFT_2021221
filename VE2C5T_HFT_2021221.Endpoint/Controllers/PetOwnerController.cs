88058154e4d8e4425e7041108bda8b2a747cdda1
VE2C5T <szilagyi.peter98@stud.uni-obuda.hu>
2021-11-27 21:19:10 +0100

    Created VetController and PetOwnerController.

---
+++ VE2C5T_HFT_2021221.Endpoint/Controllers/PetOwnerController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VE2C5T_HFT_2021221.Logic;
+using VE2C5T_HFT_2021221.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VE2C5T_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PetOwnerController : ControllerBase
+    {
+        IPetOwnerLogic petOwnerLogic;
+
+        public PetOwnerController(IPetOwnerLogic petOwnerLogic)
+        {
+            this.petOwnerLogic = petOwnerLogic;
+        }
+
+        // GET: /petowner
+        [HttpGet]
+        public IEnumerable<PetOwner> Get()
+        {
+            return petOwnerLogic.ReadAll();
+        }
+
+        // GET petowner/5
+        [HttpGet("{id}")]
+        public PetOwner Get(int id)
+        {
+            return petOwnerLogic.Read(id);
+        }
+
+        // POST /petowner
+        [HttpPost]
+        public void Post([FromBody] PetOwner value)
+        {
+            petOwnerLogic.Create(value);
+        }
+
+        // PUT api/<PetOwnerController>/5
+        [HttpPut]
+        public void Put([FromBody] PetOwner value)
+        {
+            petOwnerLogic.Update(value);
+        }
+
+        // DELETE api/<PetOwnerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            petOwnerLogic.Delete(id);
+        }
+    }
+}
---
+++ VE2C5T_HFT_2021221.Endpoint/Controllers/VetController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VE2C5T_HFT_2021221.Logic;
+using VE2C5T_HFT_2021221.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VE2C5T_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class VetController : ControllerBase
+    {
+        IVetLogic vetLogic;
+
+        public VetController(IVetLogic vetLogic)
+        {
+            this.vetLogic = vetLogic;
+        }
+
+        // GET: /vet
+        [HttpGet]
+        public IEnumerable<Vet> Get()
+        {
+            return vetLogic.ReadAll();
+        }
+
+        // GET vet/5
+        [HttpGet("{id}")]
+        public Vet Get(int id)
+        {
+            return vetLogic.Read(id);
+        }
+
+        // POST /vet
+        [HttpPost]
+        public void Post([FromBody] Vet value)
+        {
+            vetLogic.Create(value);
+        }
+
+        // PUT /vet
+        [HttpPut]
+        public void Put([FromBody] Vet value)
+        {
+            vetLogic.Update(value);
+        }
+
+        // DELETE vet/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            vetLogic.Delete(id);
+        }
+    }
+}
